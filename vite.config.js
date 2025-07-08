3a222839a67920173ad7ef0b1a3323702c028f7b
ganapathy214 <gananata214@gmail.com>
2025-07-08 17:36:29 +0530

    init

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  base: "/portfolio/", // important!
+  base: "/portfolio", // important!
   plugins: [react(), tailwindcss()],
 });
