8a8884be098dd9ebd0ec6f011b45ec9b0e0463f0
ganapathy214 <gananata214@gmail.com>
2026-06-08 18:00:17 +0530

    Fix: Set dynamic base path for Vercel vs GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  base: "/portfolio", // important!
+  base: process.env.VERCEL ? "/" : "/portfolio", // Dynamic base path for Vercel vs GH Pages
   plugins: [react(), tailwindcss()],
 });
